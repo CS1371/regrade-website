bfbf17ce7329806584fad5a667aeeaf3316f2390
Alexander Lobo <alex.lobo@gatech.edu>
2018-01-14 17:31:34 -0500

    Spring 2018 set-up

--- other/buildJSON/buildAllJSON.m
+++ other/buildJSON/buildAllJSON.m
@@ -1,11 +1,11 @@
 function buildAllJSON
 
 % Change this to the current semester
-nameListFilename = 'Name_List_Spring_2017.xlsx';
+nameListFilename = 'Name_List_Spring_18.xlsx';
 buildTAJson(nameListFilename);
 
-% Get this file from TSquare!
-gradeFileName = 'grades.csv';
+% Get this file from Canvas (and format like grades.csv from t-square)!
+gradeFileName = 'grades.xlsx';
 buildClassJson(gradeFileName);
 
 end
