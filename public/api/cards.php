0f0d9bb830b5ffcce53b975265655e4e2336adfd
Baran Usluel <busluel3@gatech.edu>
2018-10-29 01:20:15 -0400

    Add simple REST API

---
+++ public/api/cards.php
@@ -0,0 +1,45 @@
+<?php
+// api returns json
+header("Content-Type: application/json; charset=UTF-8");
+
+// check http request type
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        if (isset($_GET['name']))
+            $output = getCards($_GET['name']);
+        else
+            $output = getCards('');
+        break;
+    case 'POST':
+        // get posted data
+        $data = json_decode(file_get_contents("php://input"));
+        $output = addCard($data);
+        break;
+}
+
+exit(json_encode($output));
+
+
+function getCards($student_name) {
+    $output = (Object) array();
+    if (empty($student_name)) {
+        $output->cards = [];
+        return $output;
+    }
+    // return dummy json for now
+    $output->cards = array(
+        array("id" => 1, "name" => "HW 1 Original - " . $student_name, "student" => $student_name, "ta" => "Baran"),
+        array("id" => 2, "name" => "HW 3 Resubmission - " . $student_name, "student" => $student_name, "ta" => "Some TA"),
+    );
+    return $output;
+}
+
+function addCard($data) {
+    // assume new card is added to Trello
+    // return confirmation output
+    $output = (Object) array();
+    $output->id = 3;
+    $output->time = time();
+    return $output;
+}
+?>
--- public/api/login.php
+++ public/api/login.php
@@ -1,18 +1,6 @@
 <?php
-
-/**
- *   Example for a simple cas 2.0 client
- *
- * PHP Version 5
- *
- * @file     example_simple.php
- * @category Authentication
- * @package  PhpCAS
- * @author   Joachim Fritschi <jfritschi@freenet.de>
- * @author   Adam Franco <afranco@middlebury.edu>
- * @license  http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
- * @link     https://wiki.jasig.org/display/CASC/phpCAS
- */
+// api returns json
+header("Content-Type: application/json; charset=UTF-8");
 
 // Load the settings from the central config file
 //require_once 'config.php';
@@ -42,22 +30,5 @@ phpCAS::forceAuthentication();
 // at this step, the user has been authenticated by the CAS server
 // and the user's login name can be read with phpCAS::getUser().
 
-// logout if desired
-if (isset($_REQUEST['logout'])) {
-	phpCAS::logout();
-}
-
-// for this test, simply print that the authentication was successfull
+exit(json_encode(array("gtUsername" => phpCAS::getUser())));
 ?>
-<html>
-  <head>
-    <title>phpCAS simple client</title>
-  </head>
-  <body>
-    <h1>Successful Authentication!</h1>
-    <?php //require 'script_info.php' ?>
-    <p>the user's login is <b><?php echo phpCAS::getUser(); ?></b>.</p>
-    <p>phpCAS version is <b><?php echo phpCAS::getVersion(); ?></b>.</p>
-    <p><a href="?logout=">Logout</a></p>
-  </body>
-</html>
