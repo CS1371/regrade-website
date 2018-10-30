460faa7fc36a98a93fc32cf8d2b9c7f4cfa1e52a
Juliana <jpetrillo3@gatech.edu>
2018-10-30 16:03:21 -0400

    Add php file.

---
+++ getLink.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Function to generate the url to a particular student's submission.
+ *
+ * COURSE ID: 26266
+ *
+ * inputs: an assignment name or ID and a student's name
+ * outputs: the url that would lead to that student's submission of that
+ * assignment.
+ */
+
+ /*
+  * STEP 1: Get all submissions for the assignment.
+  * /api/v1/courses/26266/assignments/:ID/submissions
+  * tried to get with curl and in browser, kept getting error
+  * "The specified resource does not exist."
+  */
+
+ /*
+  * STEP 2: Find the students name in the list (?) of submissions
+  */
+
+ /*
+  * STEP 3: With the submission ID, I could then easily contruct the link
+  */
+
+ ?>
