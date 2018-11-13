3f310513958ef2032344e7cba3f78ffac2d3e691
Juliana <jpetrillo3@gatech.edu>
2018-11-13 11:01:00 -0500

    Get profile of matching user.

--- getLink.php
+++ getLink.php
@@ -37,9 +37,6 @@ function getLink($student, $assignment) {
     curl_setopt($ch, CURLOPT_URL, $url_assignments);
     $assignments = json_decode(curl_exec($ch), true);
 
-// Close
-    curl_close($ch);
-
 // For testing purposes only
     echo json_encode($users);
     echo json_encode($assignments);
@@ -47,16 +44,36 @@ function getLink($student, $assignment) {
 // STEP 2: ITERATRE THROUGH STUDENTS LOOKING FOR $STUDENT
 
     // get student's first initial and last name from gt username
+    $name = strtok($student, "1234567890");
+    echo($name);
+    $first = substr($name, 0, 1);
+    $last = substr($name, 1);
+    echo($first);
+    echo($last);
 
-    foreach($users as $val) {
+
+    foreach($users as $user)
+    {
         /* if first initial and last name match, get student's login id and see
          * if it matches gt username. If it matches get students id number and break.
          *
          * /api/v1/users/:user_id/profile
          */
+
+        $curName = $user['name'];
+        $curFirst = strtok($curName, " ");
+        $curLast = strtok($curFirst, " ");
+        if (substr($curFirst, 0, 1) == $first && $curLast == $last)
+        {
+            $id = $user['id'];
+            $profile_url = "https://gatech.instructure.com/api/v1/users/$id/profile?access_token=2096~9RXlwVUP3OIkybSXxvCMEiPAoGymux2IxRd3hifZEuREmJEa8x1MjmvNXeCLaCHB";
+            curl_setopt($ch, CURLOPT_URL, $profile_url);
+            $profile = json_decode(curl_exec($ch), true);
+            echo(json_encode($profile));
+        }
     }
 
-    foreach($assignments as $val) {
+    foreach($assignments as $assignment) {
         /* Match assignment number and either original or resubmission, get
          * assignment id
          *
