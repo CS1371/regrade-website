68b469ca5d8a3bff2e9ad8b48bf4b2e9d0cf62ea
Baran Usluel <busluel3@gatech.edu>
2018-11-18 21:14:38 -0500

    Add constants file

---
+++ public/api/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+// Comment this for the live server
+$isLocalhost = true;
+// Uncomment this for the live server
+//$isLocalhost = false;
+
+$canvasToken = <TOKEN HERE>;
+
+$trelloToken = <TOKEN HERE>;
+
+?>
