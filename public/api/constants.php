2725d3196c30bfa83a02ee22ed8cbe95c7204b52
Baran Usluel <busluel3@gatech.edu>
2018-11-18 21:43:03 -0500

    Add warning about skip-worktree

--- public/api/constants.php
+++ public/api/constants.php
@@ -1,4 +1,17 @@
 <?php
+#################################################################
+# WARNING - READ ME                                             #
+# -----------------                                             #
+# Before you make any changes to this file,                     #
+# run the following command (only needs                         #
+# to be done once for the local repository):                    #
+#                                                               #
+#   git update-index --skip-worktree public/api/constants.php   #
+#                                                               #
+# This will prevent git from pushing your                       #
+# local changes, containing any secret keys,                    #
+# to our remote repository.                                     #
+#################################################################
 
 // Comment this for the live server
 $isLocalhost = true;
