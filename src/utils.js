a7370b3a551eb8ad5a4a8a7c21805ca913da8264
Alexander Rao <alexhrao@gmail.com>
2019-12-24 16:20:15 -0600

    Convert utils to typescript

--- src/utils.js
+++
@@ -1,5 +0,0 @@
-export function formatDate(dateStr) {
-    var options = { year: 'numeric', month: 'short', day: 'numeric' };
-    var date = new Date(dateStr);
-    return date.toLocaleDateString("en-US", options);
-}
